84a7f63767a9c832fe3d9f77bef5afc4985cbe53
kseito <torinokonoko@gmail.com>
2022-01-16 19:20:29 +0900

    resolve 002

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,20 @@ mod tests;
 use proconio::input;
 
 fn main() {
-    answer001()
+    answer002()
+}
+
+fn answer002() {
+    input! {
+        a: i32,
+        b: i32,
+        c: i32,
+    }
+    println!("{}", calc002(a, b, c));
+}
+
+fn calc002(a: i32, b: i32, c: i32) -> i32 {
+    return a + b + c;
 }
 
 fn answer001() {
--- src/tests.rs
+++ src/tests.rs
@@ -1,10 +1,16 @@
 #[cfg(test)]
 mod tests {
-    use crate::{calc001};
+    use crate::{calc001, calc002};
 
     #[test]
     fn test_answer001() {
         let ans = calc001(7);
         assert_eq!(ans, 12);
+    }
+
+    #[test]
+    fn test_answer002() {
+        let ans = calc002(5, 6, 7);
+        assert_eq!(ans, 18);
     }
 }
